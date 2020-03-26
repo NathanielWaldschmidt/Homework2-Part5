5baec6f1091e536f7b8112a162362eda7e33f420
NathanielWaldschmidt <nwaldschmidt@student.waubonsee.edu>
2020-03-26 12:53:56 -0500

    Determines if Two Numbers are Equal or Which is Greater than the Other

--- Homework2.py
+++ Homework2.py
@@ -2,6 +2,8 @@ num1 = int(input("Enter a Number : "))
 num2 = int(input("Enter another Number : "))
 
 if num1 > num2:
-    print("num 1 is bigger than num2")
+    print("Number 1 is Bigger than Number 2")
 if num1 < num2:
-    print("num 2 is bigger than num 1")
+    print("Number 2 is Bigger than Number 1")
+if num2 == num2:
+    print("Number 1 and Number 2 are Equal")
